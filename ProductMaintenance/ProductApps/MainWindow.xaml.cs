b879b0b1ded0cfc79b7bca4271df5a67dbbaa429
Darryl Merritt <djm2020@djmnet.net.au>
2022-04-08 10:22:25 +0930

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,10 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+
+                totalChargeTextBlock.Text = (cProduct.TotalPayment + 25.00m).ToString();
+
+
             }
             catch (FormatException)
             {
